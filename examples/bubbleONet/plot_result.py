48bb06f53d976d741e824a8db255dc4bff6441ef
Yunhao Zhang <yhzhang0116@gmail.com>
2025-01-09 15:34:19 -0500

    plot

--- examples/bubbleONet/plot_result.py
+++ examples/bubbleONet/plot_result.py
@@ -9,10 +9,10 @@ plt.rcParams['font.size']=10
 
 cm = 1/2.54
 
-Bubble = np.genfromtxt("results/RP_R1.000e-05_fa2.140e+04_pa1.320e+05.txt", delimiter=" ")
+Bubble = np.genfromtxt("results/freq_0_amp_0_radius_0/results/RP_R5.000e-05_fa1.755e+06_pa1.000e+05.txt", delimiter=" ")
 
 # fig1 = plt.figure(figsize=(30*cm,30*cm))
-fig1, ax3 = plt.subplots(figsize=(25*cm, 15*cm)) 
+fig1, ax1 = plt.subplots(figsize=(20,10)) #(25*cm, 15*cm) 
 # fig1 = plt.figure()
 # numofsubplot = (3,1)
 # ax1 = plt.subplot2grid(numofsubplot,(0,0),colspan=1)
@@ -20,10 +20,10 @@ fig1, ax3 = plt.subplots(figsize=(25*cm, 15*cm))
 # ax3 = plt.subplot2grid(numofsubplot,(2,0),colspan=1)
 # plt.subplots_adjust(wspace=1.2*cm,hspace=1.2*cm)
 
-# ax1.set(xlabel=r'$t$ [$\mu$s]',ylabel=r'$R(t)$ [$\mu$m]')
-# ax1.set_xlim(xmin=0,xmax=60)
-# ax1.grid(color='gainsboro', linestyle='-', linewidth=0.5)
-# ax1.plot(Bubble[:, 1]*1e6, Bubble[:, 3]*1e6, linestyle='solid', linewidth=1,color='steelblue')
+ax1.set(xlabel=r'$t$ [$\mu$s]',ylabel=r'$R(t)$ [$\mu$m]')
+ax1.set_xlim(xmin=0,xmax=60)
+ax1.grid(color='gainsboro', linestyle='-', linewidth=0.5)
+ax1.plot(Bubble[:, 1]*1e6, Bubble[:, 3]*1e6, linestyle='solid', linewidth=1,color='steelblue')
 
 # ax2.set(xlabel=r'$t$ [$\mu$s]',ylabel=r'$\dot{R}(t)$[m/s]')
 # ax2.set_xlim(xmin=0,xmax=60)
@@ -31,11 +31,11 @@ fig1, ax3 = plt.subplots(figsize=(25*cm, 15*cm))
 # ax2.plot(Bubble[:, 1]*1e6, Bubble[:, 4], linestyle='solid', linewidth=1,color='steelblue')
 
 # ax3.set(xlabel=r'$t$ [$\mu$s]',ylabel=r'$p_\mathrm{G}(t)$ [kPa]')
-ax3.set_xlabel(r'$t$ [$\mu$s]', fontsize=20)
-ax3.set_ylabel(r'$p_\mathrm{G}(t)$ [kPa]', fontsize=20)
-ax3.set_xlim(xmin=0,xmax=60)
-ax3.grid(color='gainsboro', linestyle='-', linewidth=0.5)
-ax3.plot(Bubble[:, 1]*1e6, Bubble[:, -1]/1e3, linestyle='solid', linewidth=2.0,color='steelblue')
+#ax3.set_xlabel(r'$t$ [$\mu$s]', fontsize=20)
+#ax3.set_ylabel(r'$p_\mathrm{G}(t)$ [kPa]', fontsize=20)
+#ax3.set_xlim(xmin=0,xmax=60)
+#ax3.grid(color='gainsboro', linestyle='-', linewidth=0.5)
+#ax3.plot(Bubble[:, 1]*1e6, Bubble[:, -1]/1e3, linestyle='solid', linewidth=2.0,color='steelblue')
 
 # ax1.xaxis.set_label_coords(0.5,-0.24)
 # ax2.xaxis.set_label_coords(0.5,-0.24)
@@ -46,7 +46,7 @@ ax3.plot(Bubble[:, 1]*1e6, Bubble[:, -1]/1e3, linestyle='solid', linewidth=2.0,c
 # ax3.yaxis.set_label_coords(-0.25, 0.5)
 
 folder_path = "plots"
-file_name = "ultrasound_valiONet.png"
+file_name = "1.755e+06.png"
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 full_path = os.path.join(folder_path, file_name)
